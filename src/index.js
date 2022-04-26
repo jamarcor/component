97c1d5d803d7e5bdc8efb008f9a0fe487cf11639
James Correa <james.correa9090@gmail.com>
2022-04-26 09:27:40 -1000

    added components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import Star from "./Star";
+import Rating from "./Rating";
 
 function App() {
   return (
     <div className="App">
       <h2>Star Components:</h2>
+      <Star color="pink" isFilled />
+      <Star color="magenta" />
+      <Star color="idigo" isFilled />
+      <h2> Rating Component (uses Stars):</h2>
+      <Rating stars={4} />
     </div>
   );
 }
